09b3c878ea3ba7fab7abdd6eb18f6d1ead567820
Ashwani Kumar <62863003+ashwani361@users.noreply.github.com>
2022-01-04 08:03:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (42.02%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        long long t = 1;
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return 0;
+        }
+        while(t<n){
+            t*=2;
+        }
+        if(t==n){
+            return t-1;
+        }
+        return t-n-1;
+    }
+};
