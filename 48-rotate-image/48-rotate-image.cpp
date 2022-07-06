6cb5b16b8d1e7cbe446bfeaace60b86c31500bcd
Ashwani Kumar <62863003+ashwani361@users.noreply.github.com>
2022-07-06 10:00:46 +0530

    Time: 4 ms (41.67%), Space: 7.1 MB (33.28%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int>>& v) {
         int n = v.size(), m =v.size();
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<m; j++){
+            for(int j=i; j<m; j++){
                 swap(v[i][j], v[j][i]);
             }
         }
