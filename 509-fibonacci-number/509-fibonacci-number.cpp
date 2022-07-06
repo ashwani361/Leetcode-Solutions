70d8f732753adb1108d95baf827a13a89ae9c93a
Ashwani Kumar <62863003+ashwani361@users.noreply.github.com>
2022-07-06 09:15:12 +0530

    Time: 3 ms (60.76%), Space: 6 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        vector<int>f(35, 0);
+        f[0] = 0;
+        f[1] = 1;
+        for(int i=2; i<=30; i++){
+            f[i] = f[i-1]+f[i-2];
+        }
+        return f[n];
+    }
+};
